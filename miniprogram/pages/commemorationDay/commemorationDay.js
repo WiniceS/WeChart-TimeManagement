4125a989b21e6a7c4bc1421ac8510faa48be7f1f
WiniceS <wiphoton@163.com>
2020-05-21 22:05:17 +0800

    Initial Commit

---
+++ miniprogram/pages/commemorationDay/commemorationDay.js
@@ -0,0 +1,160 @@
+// miniprogram/pages/commemorationDay/commemorationDay.js
+const date = new Date()
+const years = []
+const months = []
+const days = []
+
+for (let i = 1990; i <= date.getFullYear(); i++) {
+  years.push(i)
+}
+
+for (let i = 1; i <= 12; i++) {
+  months.push(i)
+}
+
+for (let i = 1; i <= 31; i++) {
+  days.push(i)
+}
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    commenorationDayList: [{
+      name: 'xxx纪念日',
+      date: '2020/05/21',
+      lastInterval: 100,
+      nextInterval: 265,
+    }, {
+      name: 'xxx纪念日',
+      date: '2020/05/21',
+      lastInterval: 100,
+      nextInterval: 265,
+    }, {
+      name: 'xxx纪念日',
+      date: '2020/05/21',
+      lastInterval: 100,
+      nextInterval: 265,
+    }, {
+      name: 'xxx纪念日',
+      date: '2020/05/21',
+      lastInterval: 100,
+      nextInterval: 265,
+    }, {
+      name: 'xxx纪念日',
+      date: '2020/05/21',
+      lastInterval: 100,
+      nextInterval: 265,
+    }],
+    years,
+    year: date.getFullYear(),
+    months,
+    month: 2,
+    days,
+    day: 2,
+    value: [9999, 1, 1],
+    isDaytime: true,
+  },
+  upper(e) {
+    console.log(e)
+  },
+
+  lower(e) {
+    console.log(e)
+  },
+
+  scroll(e) {
+    console.log(e)
+  },
+
+  scrollToTop() {
+    this.setAction({
+      scrollTop: 0
+    })
+  },
+
+  tap() {
+    for (let i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1],
+          scrollTop: (i + 1) * 200
+        })
+        break
+      }
+    }
+  },
+
+  tapMove() {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  },
+  bindChange(e) {
+    const val = e.detail.value
+    this.setData({
+      year: this.data.years[val[0]],
+      month: this.data.months[val[1]],
+      day: this.data.days[val[2]],
+      isDaytime: !val[3]
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
